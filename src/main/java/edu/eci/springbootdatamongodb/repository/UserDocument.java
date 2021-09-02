bd3a6d9b9573e62e331f147880efe3077109c929
StivenVanegas <nevits28@hotmail.com>
2021-09-02 13:32:18 -0500

    added user service

--- src/main/java/edu/eci/springbootdatamongodb/repository/UserDocument.java
+++ src/main/java/edu/eci/springbootdatamongodb/repository/UserDocument.java
@@ -1,5 +1,7 @@
 package edu.eci.springbootdatamongodb.repository;
 
+import edu.eci.springbootdatamongodb.dto.UserDto;
+import edu.eci.springbootdatamongodb.model.User;
 import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -18,7 +20,68 @@ public class UserDocument {
     
     String lastName;
     Date createdAt;
+
+    public UserDocument() {
+    }
+
+    public UserDocument(String id, String name, String email, String lastName, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
+        this.createdAt = createdAt;
+    }
     
-    public UserDocument(){
+    public UserDocument(User user){
+        this.name = user.getName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.createdAt = user.getCreatedAt();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    public void update(UserDto userDto){
+        this.name = userDto.getName();
+        this.lastName = userDto.getLastName();
+        this.email = userDto.getEmail();
     }
 }
